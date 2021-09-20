96cd19afb622dabb4a36e397f46d30cde0515d73
Thai B1706641 <thaib1706641@student.ctu.edu.vn>
2021-09-21 05:10:12 +0700

    vendor update

--- models/certificateModel.js
+++ models/certificateModel.js
@@ -15,11 +15,27 @@ let select_byissuer = (issuer_id) => {
     );
   });
 };
+let select_byNumber = (number) => {
+  return new Promise((resolve, reject) => {
+    conn.query(
+      "SELECT * FROM certificates WHERE number=?",
+      number,
+      function (err, results) {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(results);
+        }
+      }
+    );
+  });
+};
 
 let insert = (data) => {
   return new Promise((resolve, reject) => {
-    conn.query("INSERT INTO issuer SET ?", data, function (err) {
+    conn.query("INSERT INTO certificates SET ?", data, function (err) {
       if (err) {
+        console.log(this.sql);
         reject(err);
       } else {
         resolve("A new row has been created!");
@@ -31,7 +47,7 @@ let insert = (data) => {
 let update = (number, issuer_id, data) => {
   return new Promise((resolve, reject) => {
     conn.query(
-      "UPDATE certificates SET ? WHERE issuer_id=?, number=?",
+      "UPDATE certificates SET ? WHERE issuer_id=? and number=?",
       [data, issuer_id, number],
       function (err) {
         if (err) {
@@ -43,9 +59,60 @@ let update = (number, issuer_id, data) => {
     );
   });
 };
+let update_ipfs_hash = (number, ipfs_hash) => {
+  return new Promise((resolve, reject) => {
+    conn.query(
+      "UPDATE certificates SET ipfs_hash = ?, status='Done'  WHERE number=?",
+      [ipfs_hash, number],
+      function (err, results) {
+        if (err) {
+          console.log(this.sql);
+          reject(err);
+        } else {
+          resolve("A row has been deleted!");
+        }
+      }
+    );
+  });
+};
+let delete_byNumber = (number) => {
+  return new Promise((resolve, reject) => {
+    conn.query(
+      "DELETE FROM certificates WHERE number=?",
+      [number],
+      function (err, results) {
+        if (err) {
+          console.log(this.sql);
+          reject(err);
+        } else {
+          resolve("A row has been deleted!");
+        }
+      }
+    );
+  });
+};
+let get_certformipfs = (url) => {
+  return new Promise((resolve, reject) => {
+    http.get("http://ipfs.io/ipfs/" + ipfs_hash, function (res) {
+      let data = "",
+        json_data;
+      res.on("data", function (stream) {
+        data += stream;
+      });
+      res.on("end", function () {
+        json_data = JSON.parse(data);
+        resolve(json_data);
+      });
+    });
+    reject("Fail")
+  });
+};
 
 module.exports = {
   select_byissuer,
+  select_byNumber,
   insert,
   update,
+  update_ipfs_hash,
+  delete_byNumber,
 };
--- routes/issuer/function.js
+++ routes/issuer/function.js
@@ -1,44 +1,154 @@
 const express = require("express");
 const passport = require("passport");
 const router = express.Router();
-const fs = require('fs')
+const fs = require("fs");
 const moment = require("moment");
+const certModel = require("../../models/certificateModel");
 
-router.get('/',(req, res) => {
-  res.render('./issuer');
-})
-router.get('/certificate/create', (req, res) => {
-  
-  res.render('./issuer/certificate/create');
-})
-router.get('/certificate', (req, res) => {
-  try{
-    require("../../models/certificateModel").select_byissuer("ISR0001").then(function(data){
-      // console.log(data);
-      res.render('./issuer/certificate/',{cert_list: data, moment});
-    })
-  }catch(err){
+const ipfsClient = require("ipfs-http-client");
+const { Certificate } = require("crypto");
+
+const projectId = process.env.PROJECT_ID;
+const projectSecret = process.env.PROJECT_SECRET;
+
+const auth =
+  "Basic " + Buffer.from(projectId + ":" + projectSecret).toString("base64");
+
+const client = ipfsClient.create({
+  host: "ipfs.infura.io",
+  port: 5001,
+  protocol: "https",
+  headers: {
+    authorization: auth,
+  },
+});
+
+client.pin.add("QmeGAVddnBSnKc1DLE7DLV9uuTqo5F7QbaveTjr45JUdQn").then((res) => {
+  // console.log(res)
+});
+
+router.get("/", (req, res) => {
+  res.render("./issuer");
+});
+router.get("/certificate/create", (req, res) => {
+  res.render("./issuer/certificate/create");
+});
+router.get("/certificate", (req, res) => {
+  try {
+    certModel.select_byissuer("ISR0001").then(function (data) {
+      res.render("./issuer/certificate/", { cert_list: data, moment });
+    });
+  } catch (err) {
     console.log(err);
     // res.redirect("/issuer");
   }
-})
-router.get('/certificate/edit', (req, res) => {
-  res.render('./issuer/certificate/edit');
-})
+});
+router.get("/certificate/edit", (req, res) => {
+  res.render("./issuer/certificate/edit");
+});
 
-router.post('/create_certificate', (req, res) => {
+router.get("/certificate/detail", (req, res) => {
+  // lay du lieu
+  let cert_info;
+  try {
+    certModel.select_byNumber(req.query.number).then(async function (data) {
+      var filename = data[0].filename;
+      if (filename != null ){
+        var file  = fs.readFileSync('./public/cert/'+filename);
+        cert_info = JSON.parse(file);
+      }else{
+        
+      }
+    });
+  } catch (err) {
+    console.log(err);
+  }
+});
+//
+router.get("/certificate/delete", async (req, res) => {
+  // lay ten file
+  try {
+    certModel.select_byNumber(req.query.number).then(function (data) {
+      var cert = data[0];
+      if (cert.ipfs_hash == null) {
+        fs.unlink("./public/cert/" + cert.filename, (err) => {
+          // xoa file
+          // Xoá thông tin trong csdl
+          if (err) console.log(err);
+          else {
+            try {
+              certModel.delete_byNumber(req.query.number);
+              res.redirect("/issuer/certificate/");
+            } catch (err) {
+              console.log(err);
+            }
+          }
+        });
+      } else {
+        console.log("File đã được up lên ipfs");
+      }
+    });
+  } catch (err) {
+    console.log(err);
+  }
+});
+router.get("/certificate/up_to_ipfs", async (req, res) => {
+  // lấy file
+  try {
+    filename = certModel
+      .select_byNumber(req.query.number)
+      .then(async function (data) {
+        var filename = data[0].filename;
+        const file = await client.add(
+          ipfsClient.globSource("./public/cert/" + filename)
+        );
+        var ipfs_hash = file.cid.toString();
+        // luu vao csdl
+        try {
+          await certModel.update_ipfs_hash(req.query.number, ipfs_hash);
+        } catch (err) {
+          console.log(err);
+        }
+        // res.flash(msg,"Đã tải chứng chỉ lên ipfs!");
+        res.redirect("/issuer/certificate");
+      });
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+router.post("/certificate/create", async (req, res) => {
   var data = req.body;
-  console.log(data);
-  var fname = 'cert_'+data.number+'_'+Date.now()+'.json'; 
-  fs.appendFile('./public/cert/cert_'+fname, JSON.stringify(data), function(err) {
-    if (err){
-      console.log(err);
-    }else {
-       
+  var fname = "cert_" + data.number + "_" + Date.now() + ".json";
+  // Tạo file - them vao CSDL - put ipfs
+  var cert = {
+    number: data.number,
+    issuer_id: "ISR0001",
+    filename: fname,
+    status: "Đang kiểm tra",
+  };
+
+  fs.appendFile(
+    "./public/cert/" + fname,
+    JSON.stringify(data),
+    async function (err) {
+      if (err) {
+        console.log(err);
+      } else {
+        res.redirect("/issuer/certificate");
+        try {
+          certModel.insert(cert);
+          res.redirect("/issuer/certificate");
+        } catch (err) {
+          console.log(err);
+        }
+      }
     }
-  })
+  );
+  try {
+  } catch (err) {
+    console.log(err);
+  }
+});
 
-})
-
-
-module.exports = router
+module.exports = router;
